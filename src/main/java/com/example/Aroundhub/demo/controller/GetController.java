3bef73f90b368db232356a98609c798b010e6910
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2022-01-04 00:36:26 +0900

    getAPI

---
+++ src/main/java/com/example/Aroundhub/demo/controller/GetController.java
@@ -0,0 +1,53 @@
+package com.example.Aroundhub.demo.controller;
+
+import com.example.Aroundhub.demo.dto.MemberDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String getHello(){
+        return "Hello World";
+    }
+
+    @GetMapping(value="/name")
+    public String getName(){
+        return "Flature";
+    }
+
+    @GetMapping(value="/variable1/{variable}")
+    public String getVariable(@PathVariable("variable") String variable){
+        return variable;
+    }
+
+    @GetMapping(value="/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    @GetMapping(value="/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ){
+        return name+" "+email+" "+organization;
+    }
+
+    @GetMapping(value="/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb= new StringBuilder();
+        param.entrySet().forEach(map->{
+           sb.append(map.getKey()+" : "+map.getValue()+"\n");
+        });
+        return sb.toString();
+    }
+
+    @GetMapping(value="/request3")
+    public String getRequestParam3(MemberDTO dto){
+        return dto.toString();
+    }
+}
--- src/main/java/com/example/Aroundhub/demo/controller/HelloController.java
+++ src/main/java/com/example/Aroundhub/demo/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.Aroundhub.demo.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value="/hello", method= RequestMethod.GET)
-    public String hello(){
+    @GetMapping(value="/hello")
+    public String hello() {
         return "hello world";
     }
 }
